f45716011cc4c87651049806e77c207d4f69abe0
aboodabdo347 <aboodabdo347@gmail.com>
2024-01-22 15:00:35 +0300

    fixing and add some logic to the game

--- script.js
+++ script.js
@@ -14,16 +14,83 @@ for (let i = 0; i < 10; i++) {
   arrC[i] = []
   for (let j = 0; j < 10; j++) {
     arrC[i][j] = ''
+    if (i === 5 && j === 5) {
+      arrC[i][j] = 'wall'
+    }
   }
 }
 class Character {
-  constructor(name, positionX, positionY, team) {
+  constructor(name, positionX, positionY, team,type) {
     this.name = name
     this.positionX = positionX
     this.positionY = positionY
     this.health = 100
     this.team = team
     this.dead = 0
+    this.type=type
+    this.bullets=0
+    this.first=0
+  }
+  getType(){
+    return this.type
+  }
+  getFirst(){
+    return this.first
+  }
+  setFirst(num){
+this.first=num
+  }
+  setBullets(){
+    if(this.getType()==="infantryman" && this.getFirst()===0){
+this.bullets=30
+this.setFirst(1)
+    }
+    else if(this.getType()==="tank" && this.getFirst()===0){
+      this.bullets=5
+      this.setFirst(1)
+          }
+          else if(this.getType()==="sniper" && this.getFirst()===0){
+            this.bullets=10
+            this.setFirst(1)
+                }
+if(this.getType()==="infantryman" && this.getFirst()===1){
+  this.bullets++
+}
+else if(this.getType()==="tank" && this.getFirst()===1){
+  this.bullets++
+}
+else if(this.getType()==="sniper" && this.getFirst()===1){
+  this.bullets++
+}
+  }
+  getBullets(){
+    return this.bullets
+  }
+  reload(){
+    if (this.getBullets()<5 && this.getType()==="infantryman"){
+      setInterval(()=>{
+        if(this.getBullets()<31){
+          this.setBullets()
+        }
+      },100)
+    }
+   else if (this.getBullets()<1 && this.getType()==="tank"){
+      setInterval(()=>{
+        if(this.getBullets()<6){
+          this.setBullets()
+        }
+      },400)
+    }
+    else if (this.getBullets()<4 && this.getType()==="sniper"){
+      setInterval(()=>{
+        if(this.getBullets()<11){
+          this.setBullets()
+        }
+      },200)
+    }
+  }
+  getHealth() {
+    return this.health
   }
   getName() {
     return this.name
@@ -46,7 +113,7 @@ class Character {
     if (this.health === 0) {
       boxes[this.positionX * 10 + this.positionY].innerHTML = 'rip'
       arrC[this.positionX][this.positionY] = 'rip'
-      this.dead++
+      this.dead = 1
     }
   }
 
@@ -108,13 +175,107 @@ class Character {
       this.setPosition(nextX, nextY)
     }
   }
+  hide() {
+    let wallsArr = []
+    let closeX=this.getPositionX()
+    let closeY=this.getPositionY()
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if ((arrC[j][i] = 'wall')) {
+          wallsArr.push({x:j, y:i})
+        }
+      }
+    }
+    if (this.getHealth() < 25) {
+      for(let i=0;i<wallsArr;i++){
+      if(closeX>wallsArr[i].x){
+
+      }
+      }
+for (closeX;closeX<=wallsArr.length;closeX++){
+  for(closeY)
+}
+    }
+  }
+  
+reload(){}
+
+getScore(){}
+
+getCheck(){}
 
   battle(enemy) {
     if (this.isAdjacent(enemy) === 1) {
       enemy.kill()
     }
   }
-
+  moveTo(Character) {
+    let closeX = this.getPositionX()
+    let closeY = this.getPositionY()
+    for (let enemy of Character) {
+      if (enemy.getPositionX() > closeX) {
+        for (let i = closeX; i <= enemy.getPositionX(); i++) {
+          if (
+            i === enemy.getPositionX() &&
+            this.getTeam() !== enemy.getTeam() &&
+            enemy.getDead() == 0
+          ) {
+            let movetoEnemy = enemy.getPositionX()
+            for (closeX; closeX <= movetoEnemy; closeX++) {
+              this.setPosition(closeX, this.getPositionY())
+            }
+          }
+        }
+      } else if (enemy.getPositionX() < closeX) {
+        for (let i = closeX; i >= enemy.getPositionX(); i--) {
+          if (
+            i === enemy.getPositionX() &&
+            this.getTeam() !== enemy.getTeam() &&
+            enemy.getDead() == 0
+          ) {
+            let movetoEnemy = enemy.getPositionX()
+            for (closeX; closeX >= movetoEnemy; closeX--) {
+              this.setPosition(closeX, this.getPositionY())
+            }
+          }
+        }
+      } else if (enemy.getPositionY() > closeY) {
+        for (let i = closeY; i <= enemy.getPositionY(); i++) {
+          if (
+            i === enemy.getPositionY() &&
+            this.getTeam() !== enemy.getTeam() &&
+            enemy.getDead() == 0
+          ) {
+            let movetoEnemy = enemy.getPositionY()
+            for (closeY; closeY <= movetoEnemy; closeY++) {
+              this.setPosition(this.getPositionX(), closeY)
+            }
+          }
+        }
+      } else if (enemy.getPositionY() < closeY) {
+        for (let i = closeY; i >= enemy.getPositionY(); i--) {
+          if (
+            i === enemy.getPositionY() &&
+            this.getTeam() !== enemy.getTeam() &&
+            enemy.getDead() == 0
+          ) {
+            let movetoEnemy = enemy.getPositionY()
+            for (closeY; closeY >= movetoEnemy; closeY--) {
+              this.setPosition(this.getPositionX(), closeY)
+            }
+          }
+        }
+      }
+      if (
+        this !== enemy &&
+        this.getTeam() !== enemy.getTeam() &&
+        this.isAdjacent(enemy) === 1 &&
+        this.getDead() === 0
+      ) {
+        this.battle(enemy)
+      }
+    }
+  }
   findAndBattle(Character) {
     for (let enemy of Character) {
       if (
@@ -126,6 +287,11 @@ class Character {
         this.battle(enemy)
       }
     }
+  }
+  move() {
+    let gen = Math.random()
+    if (gen < 0.1) this.randomMove()
+    else this.moveTo()
   }
 }
 class X extends Character {}
@@ -155,8 +321,8 @@ document.addEventListener('DOMContentLoaded', () => {
   characters.forEach((c) => c.setPosition(c.getPositionX(), c.getPositionY()))
   setInterval(() => {
     characters.forEach((c) => {
-      c.randomMove()
+      c.move()
       c.findAndBattle(characters)
     })
-  }, 1000)
+  }, 100)
 })
